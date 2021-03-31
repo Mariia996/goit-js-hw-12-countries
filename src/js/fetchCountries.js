45f63bb2201356923afa9c735cba4c55052dca92
Mariia Hunko <masha.gunko96@gmail.com>
2021-03-31 15:19:45 +0300

    Done

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,3 @@
-
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import _ from 'lodash';
@@ -7,34 +6,40 @@ import createCountryList from '../templates/countries.hbs';
 import createCountryCard from '../templates/country-card.hbs';
 
 const inputRef = document.getElementById('name-country');
-// const countryListRef = document.getElementById('countries-list');
 const countryContRef = document.getElementById('dynamic-container');
 
 const basicUrl = 'https://restcountries.eu/rest/v2';
 
-
 inputRef.addEventListener('input',  _.debounce(function (e) {
     e.preventDefault();
 
-    const userCountry = e.target.value;
+    const userCountry = this.value;
+    if (!userCountry) {
+        countryContRef.innerHTML = "";
+        return;
+    }
     foundCountryRequest(userCountry);
 }, 500));
-
-
 
 function foundCountryRequest(country) {
     const userRequest = fetch(`${basicUrl}/name/${country}`);
 
     userRequest
         .then(response => {
-            if (!response.ok) {
-                throw new Error("Error!");
+            if (!response.ok || response.status === 404) {
+                throw new Error(response.statusText);
             }
             return response.json();
         })
-        .then(item => {
+        .then(createCountryHandler)
+        .catch(function () {
+            countryContRef.innerHTML =
+                `<p class="error-notification">Country was not found. Please, try again.</p>`;
+            });
+}
 
-             if (item.length >= 2 && item.length <= 10) {
+function createCountryHandler(item) {
+            if (item.length >= 2 && item.length <= 10) {
                 countryContRef.innerHTML = createCountryList(item);
 
             } else if (item.length === 1) {
@@ -43,19 +48,17 @@ function foundCountryRequest(country) {
 
 
             } else if (item.length > 10) {
-            countryContRef.innerHTML = '';
+                countryContRef.innerHTML = '';
 
-                const myError = error({
+                error({
                     text: "Too many matches found. Please entry a more specific query!",
                     styling: 'brighttheme',
-                    delay: 1000,
+                    icon: true,
+                    addClass: 'error-message',
+                    delay: 700,
                     hide: true,
-                    shadow: true,
-                    context: countryContRef,
                 });
-
-            }
-        })
-        .catch(error => { countryContRef.innerHTML = `<p class="error-notification">Please, try again.</p>`; });
-
+                const pnotify = document.querySelector(".pnotify");
+                countryContRef.append(pnotify);
+             }
 }
